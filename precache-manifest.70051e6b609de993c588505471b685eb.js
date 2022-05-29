7c14fd6e6e7ea0c81f0506b937391f398b93a441
MeLlamoArroz <mellamoarroz@gmail.com>
2022-05-29 17:53:00 +0300

    Updates

---
+++ precache-manifest.70051e6b609de993c588505471b685eb.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "bb3e908e040e707147c3",
+    "url": "/DataViz-FinalProject/static/css/main.5794acd6.chunk.css"
+  },
+  {
+    "revision": "bb3e908e040e707147c3",
+    "url": "/DataViz-FinalProject/static/js/main.bb3e908e.chunk.js"
+  },
+  {
+    "revision": "ad39ee43e5f1e39ec27e",
+    "url": "/DataViz-FinalProject/static/js/1.ad39ee43.chunk.js"
+  },
+  {
+    "revision": "f2d969274a204a74a21b",
+    "url": "/DataViz-FinalProject/static/js/2.f2d96927.chunk.js"
+  },
+  {
+    "revision": "0f2eeef1d03196d45a5c",
+    "url": "/DataViz-FinalProject/static/js/runtime~main.0f2eeef1.js"
+  },
+  {
+    "revision": "b4d8c41476c425538f4e96a341acd34e",
+    "url": "/DataViz-FinalProject/static/media/logo.b4d8c414.png"
+  },
+  {
+    "revision": "5d9ee8c8497412c4be5644bfd1575957",
+    "url": "/DataViz-FinalProject/index.html"
+  }
+];
